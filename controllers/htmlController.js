0f88642f2c0f5e7f9c36f59d0c543677cd382228
John Kang <pollyfactor@hotmail.com>
2020-04-27 19:10:46 -0400

    updated routes for htmlController

--- controllers/htmlController.js
+++ controllers/htmlController.js
@@ -1,46 +1,11 @@
 const router = require("express").Router();
 const db =  require("../models");
 
-router.get("/api/workouts", (req, res) => {
-    db.Workout.find({})
-      .then(workoutDb => {
-        res.json(workoutDb);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
-  
-router.put("/api/workouts/:id", (req, res) => {
-    db.Workout.findByIdAndUpdate(req.params.id, {
-      $push: {exercises: req.body},
-    }, { new: true })
-      .then(workoutDb => {
-        res.json(workoutDb);
-      }).catch(err => {
-        res.json(err);
-      });
-  });
-  
-  router.post("/api/workouts", (req, res) => {
-    console.log(req.body);
-    db.Workout.create(req.body)
-    .then(workoutDb => {
-      res.json(workoutDb);
-    }).catch(err => {
-      res.json(err);
-    });
-  });
-  
-  router.get("/api/workouts/range", (req, res) => {
-    db.Workout.find({})
-    .limit(7)
-    .then(workoutDb => {
-      res.json(workoutDb);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-  });
+router.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/exercise.html"));
+});
 
+router.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/stats.html"));
+});
   module.exports = router;
